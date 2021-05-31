a5a7327a6d698bb8d7d85e842f01dad3a4c8a709
maxim-savchuk <maxsavchuk1105@gmail.com>
2021-05-31 19:14:43 +0300

    DOM-01

---
+++ js/01-query-selectors.js
@@ -0,0 +1,5 @@
+// const navEl = document.querySelector('.first__list');
+// console.log('navEl :',navEl); // возвращает null если ничего не нашло
+
+// const navLinksEl = navEl.querySelectorAll('.first__item--link');
+// console.log( 'navLinksEl :', navLinksEl); // Возвращает пустой массив если ничего не нашло
